8f9ddcd40407d26b531e64de80dc7f7701b11361
Alex East <alexeastltd@gmail.com>
2019-05-13 22:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.error("help!")
 console.log("Remember to log this!")
-Console.error('Don\'t even try this')
-console.warn("Are you sure")
-console.warn("Dont do this")
+console.warn("Are you sure?")
+
+
